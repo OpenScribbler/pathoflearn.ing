27879c073aed4f777310fc8a1edd9d414d14dcd7
OpenScribbler <openscribbler@pm.me>
2024-10-18 22:54:09 -0700

    add and set up starlight sidebar topics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,49 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Path of Learning',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/OpenScribbler/pathoflearn.ing',
 			},
-			sidebar: [
+			plugins: [
+				starlightSidebarTopics([
 				{
-					label: 'Guides',
+					label: 'Path of Exile',
+					icon: 'moon',
+					link: '/poe/',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+						{
+							label: 'Start Here',
+							items: ['poe/overview', 'poe/prereqs'],
+						},
+						{
+							label: 'Learn PoE',
+							items: ['poe/learn/1', 'poe/learn/2'],
+						},
+					]
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Path of Exile 2',
+					icon: 'star',
+					link: '/poe2/',
+					items: [
+						{
+						label: 'Start Here',
+						items: ['poe2/overview', 'poe2/prereqs'],
+						},
+						{
+							label: 'Learn PoE2',
+							items: ['poe2/learn/1', 'poe2/learn/2'],
+						},
+						]
 				},
+				]),
 			],
 		}),
 	],
